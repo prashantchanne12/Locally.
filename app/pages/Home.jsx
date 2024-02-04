6d7d166437ec656bcdbff3e948d64dac95bece61
Prashant Channe <prashantchanne121@gmail.com>
2024-02-04 20:55:17 +0530

    Adding bottom navigation

--- app/pages/Home.jsx
+++ app/pages/Home.jsx
@@ -1,79 +1,27 @@
-import * as React from "react";
-import {
-  Animated,
-  TouchableOpacity,
-  View,
-  useWindowDimensions,
-} from "react-native";
-import { TabView, SceneMap } from "react-native-tab-view";
-import CustomText from "../components/CustomText";
-import { PRIMARY } from "@/utils/constants";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
 
-const FirstRoute = () => (
-  <View>
-    <CustomText text="First" bold />
-  </View>
-);
+const Tab = createBottomTabNavigator();
+import Services from "./Services";
+import Signin from "./Signin";
 
-const SecondRoute = () => (
-  <View>
-    <CustomText text="Second" bold />
-  </View>
-);
-
-const renderScene = SceneMap({
-  first: FirstRoute,
-  second: SecondRoute,
-});
-
-export default function TabViewExample() {
-  const layout = useWindowDimensions();
-
-  const [index, setIndex] = React.useState(0);
-  const [routes] = React.useState([
-    { key: "first", title: "First" },
-    { key: "second", title: "Second" },
-  ]);
-
-  const renderTabBar = ({ navigationState }) => {
-    const { index } = navigationState;
-    return (
-      <View className="pt-2 px-5">
-        <View>
-          <CustomText text="ServiceX" className="text-2xl" bold />
-        </View>
-        <View className="flex-row space-x-4 mt-3">
-          {navigationState.routes.map((route, i) => {
-            const isSelected = i === index;
-            return (
-              <TouchableOpacity onPress={() => setIndex(i)} key={route.title}>
-                <View
-                  className={`${
-                    isSelected
-                      ? `bg-[${PRIMARY}]`
-                      : "bg-gray-200 border border-gray-300"
-                  } px-5 py-2`}
-                >
-                  <CustomText
-                    text={route.title}
-                    className={isSelected ? "text-white" : "text-black"}
-                  />
-                </View>
-              </TouchableOpacity>
-            );
-          })}
-        </View>
-      </View>
-    );
-  };
-
+const Home = () => {
   return (
-    <TabView
-      navigationState={{ index, routes }}
-      renderScene={renderScene}
-      onIndexChange={setIndex}
-      initialLayout={{ width: layout.width }}
-      renderTabBar={renderTabBar}
-    />
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen
+          name="Home"
+          component={Services}
+          options={{ headerShown: false }}
+        />
+        <Tab.Screen
+          name="Settings"
+          component={Signin}
+          options={{ headerShown: false }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
-}
+};
+
+export default Home;
---
+++ app/pages/Services.jsx
@@ -0,0 +1,84 @@
+import * as React from "react";
+import {
+  Animated,
+  TouchableOpacity,
+  View,
+  useWindowDimensions,
+} from "react-native";
+import { TabView, SceneMap } from "react-native-tab-view";
+import CustomText from "../components/CustomText";
+import { PRIMARY } from "@/utils/constants";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const FirstRoute = () => (
+  <View>
+    <StatusBar style="dark" />
+    <CustomText text="First" bold />
+  </View>
+);
+
+const SecondRoute = () => (
+  <View>
+    <StatusBar style="dark" />
+    <CustomText text="Second" bold />
+  </View>
+);
+
+const renderScene = SceneMap({
+  first: FirstRoute,
+  second: SecondRoute,
+});
+
+export default function TabViewExample() {
+  const layout = useWindowDimensions();
+
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    { key: "first", title: "First" },
+    { key: "second", title: "Second" },
+  ]);
+
+  const renderTabBar = ({ navigationState }) => {
+    const { index } = navigationState;
+    return (
+      <SafeAreaView className="pt-2 px-5">
+        <View>
+          <CustomText text="ServiceX" className="text-2xl" bold />
+        </View>
+        <View className="flex-row space-x-4 mt-3">
+          {navigationState.routes.map((route, i) => {
+            const isSelected = i === index;
+            return (
+              <TouchableOpacity onPress={() => setIndex(i)} key={route.title}>
+                <View
+                  className={`${
+                    isSelected
+                      ? `bg-[${PRIMARY}]`
+                      : "bg-gray-200 border border-gray-200"
+                  } px-5 py-2`}
+                >
+                  <CustomText
+                    text={route.title}
+                    semibold
+                    className={isSelected ? "text-white" : "text-black"}
+                  />
+                </View>
+              </TouchableOpacity>
+            );
+          })}
+        </View>
+      </SafeAreaView>
+    );
+  };
+
+  return (
+    <TabView
+      navigationState={{ index, routes }}
+      renderScene={renderScene}
+      onIndexChange={setIndex}
+      initialLayout={{ width: layout.width }}
+      renderTabBar={renderTabBar}
+    />
+  );
+}
