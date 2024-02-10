7dc3547ef21b259ddb02af10c2cb5aed3280f41f
Prashant Channe <prashantchanne121@gmail.com>
2024-02-10 14:51:17 +0530

    Added time format

--- app/components/Card.jsx
+++ app/components/Card.jsx
@@ -5,11 +5,57 @@ import { Ionicons } from "@expo/vector-icons";
 import { PRIMARY } from "@/utils/constants";
 import { Image } from "expo-image";
 
+const date = new Date()
+  .toLocaleString("en-US", { hour12: false })
+  .split(",")[1]
+  .trim()
+  .slice(0, 5);
+
+const get12HoursTime = (time) => {
+  time = time.split(":");
+  const hour = parseInt(time[0]);
+  const minutes = time[1];
+  let finalTime = "";
+  if (hour > 12) {
+    finalTime = hour - 12 + ":" + minutes + " PM";
+  } else {
+    if (hour === 0) hour = 12;
+    finalTime = hour + ":" + minutes + " AM";
+  }
+  return finalTime;
+};
+
+const isInRange = (start, end, input) => {
+  const startMinutes = convertToMinutes(start);
+  const endMinutes = convertToMinutes(end);
+  const inputMinutes = convertToMinutes(input);
+
+  if (inputMinutes < startMinutes && inputMinutes > endMinutes) {
+    return true;
+  }
+
+  if (endMinutes < startMinutes) {
+    return inputMinutes >= startMinutes || inputMinutes <= endMinutes;
+  } else {
+    return inputMinutes >= startMinutes && inputMinutes <= endMinutes;
+  }
+};
+
+const convertToMinutes = (timeString) => {
+  var time = timeString.split(":").map(Number);
+  return time[0] * 60 + time[1];
+};
+
 const Card = ({ service }) => {
-  // TODO
-  // Conditionally show opens_at closes_at
-  // if before opens_at show = opens_at
-  // if after opens_at show = open until
+  let time = "";
+  const a = service.opens_at.slice(0, 5);
+  const b = service.closes_at.slice(0, 5);
+  if (isInRange(a, b, date)) {
+    time = "Open until " + get12HoursTime(b);
+  } else {
+    time = "Opens at " + get12HoursTime(a);
+  }
+
   return (
     <View className="border bg-white border-gray-200 px-2 pt-2 pb-4 my-2">
       <View>
@@ -39,15 +85,7 @@ const Card = ({ service }) => {
         </View>
         <View className="flex-row items-center space-x-1 text-gray-500">
           <Ionicons name="time-outline" size={18} color="black" />
-          <CustomText
-            text={
-              service.opens_at.slice(0, 5) +
-              "AM to " +
-              service.closes_at.slice(0, 5) +
-              "PM"
-            }
-            className="text-xs text-gray-500"
-          />
+          <CustomText text={time} className="text-xs text-gray-500" />
         </View>
       </View>
     </View>
